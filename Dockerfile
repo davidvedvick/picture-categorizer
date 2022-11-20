c59a1b09eb119eb6350f88591cfbf67c6fc0b85a
David Vedvick <dvedvick@gmail.com>
2022-11-19 18:56:06 -0600

    Working dockerbuild

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
-FROM eclipse-temurin:17-jdk-alpine AS build
-WORKDIR /workspace/app
+FROM eclipse-temurin:17-jdk-jammy AS build
 
-COPY . /workspace/app
+WORKDIR /workspace/app
+COPY . .
+
 RUN --mount=type=cache,target=/root/.gradle ./gradlew clean build
-RUN mkdir -p build/dependency && (cd build/dependency; jar -xf ../libs/*.jar)
+RUN cd build/libs; cp *[!plain].jar catpics.jar
+
+#RUN mkdir -p build/dependency && (cd build/dependency; jar -xf ../libs/*[!plain].jar)
 
 FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
-ARG DEPENDENCY=/workspace/app/build/dependency
-COPY --from=build ${DEPENDENCY}/BOOT-INF/lib /app/lib
-COPY --from=build ${DEPENDENCY}/META-INF /app/META-INF
-COPY --from=build ${DEPENDENCY}/BOOT-INF/classes /app
+ARG DEPENDENCY=/workspace/app/build/libs
+COPY --from=build ${DEPENDENCY} /app
+ENTRYPOINT ["java","-jar","/app/catpics.jar"]
--- src/main/kotlin/info/davidvedvick/seis739/catpics/CatPicsApplication.kt
+++ src/main/kotlin/info/davidvedvick/seis739/catpics/CatPicsApplication.kt
@@ -1,18 +1,35 @@
 package info.davidvedvick.seis739.catpics
 
 import org.flywaydb.core.Flyway
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
 
 @SpringBootApplication
-class CatPicsApplication
+class CatPicsApplication {
+
+	@Autowired private lateinit var env: Environment
+
+	@Bean
+	fun runner(context: ApplicationContext): CommandLineRunner = CommandLineRunner {
+		val dataSource = env["spring.datasource.url"]
+
+		LoggerFactory.getLogger(CatPicsApplication::class.java).info("Datasource: $dataSource")
+
+		Flyway
+			.configure()
+			.dataSource(dataSource, "cat", "scratch")
+			.load()
+			.migrate()
+	}
+}
 
 fun main(args: Array<String>) {
-	Flyway
-		.configure()
-		.dataSource("jdbc:postgresql://localhost:5432/catpics", "cat", "scratch")
-		.load()
-		.migrate()
-
 	runApplication<CatPicsApplication>(*args)
 }
