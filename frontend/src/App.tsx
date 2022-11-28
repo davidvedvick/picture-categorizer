e2f03b6c66cbae3d4666b836522d932942158603
David Vedvick <dvedvick@gmail.com>
2022-11-27 21:57:44 -0600

    Add App Navigation, submit user data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
 // import './App.css';
 import {UserLogin} from "./Users/UserLogin";
+import {BrowserRouter as Router, NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                {/*<img src={logo} className="App-logo" alt="logo" />*/}
-            </header>
-            <UserLogin/>
-        </div>
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-lg bg-light">
+                    <div className="container-fluid">
+                        <NavLink className="navbar-brand" to="/">Cat Pics!</NavLink>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <NavLink className="navbar-brand" to="/">Home</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/pictures">Browse Pictures</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="nav-link" to="/user">User Management</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+
+                <hr />
+
+                {/*
+                      A <Switch> looks through all its children <Route>
+                      elements and renders the first one whose path
+                      matches the current URL. Use a <Switch> any time
+                      you have multiple routes, but you want only one
+                      of them to render at a time
+                    */}
+                <Routes>
+                    <Route path="/">
+                    </Route>
+                    <Route path="/pictures">
+                    </Route>
+                    <Route path="/user" element={<UserLogin/>} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- frontend/src/Users/UserLogin.tsx
+++ frontend/src/Users/UserLogin.tsx
@@ -1,27 +1,44 @@
-import {Button, Col, Container, Form, FormGroup, Input, Label} from "reactstrap";
+import {useState} from "react";
 
 export function UserLogin() {
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("")
+
     async function handleSubmit() {
-        await fetch("")
+        await fetch(
+            "/api/users",
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        userName: username,
+                        password: password
+                    }
+                )
+            });
     }
 
     return (
-        <Container fluid="sm">
-            <Col sm>
-                <Form>
-                    <FormGroup floating>
-                        <Input id="user" type="text" placeholder="User Name"/>
-                        <Label for="user">User Name</Label>
-                    </FormGroup>
-                    <FormGroup floating>
-                        <Input id="password" type="password" placeholder="Password"/>
-                        <Label for="password">Password</Label>
-                    </FormGroup>
-                    {/*<p><Button type="submit">Login</Button></p>*/}
-                    <p><Button type="submit" onSubmit={handleSubmit}>Login</Button></p>
-                </Form>
-            </Col>
-        </Container>
+        <>
+            <div className="sm container-fluid">
+                <div className="sm">
+                    <form>
+                        <div className="form-floating mb-3">
+                            <input id="user" className="form-control" type="text" placeholder="User Name" onChange={(e) => setUsername(e.target.value)} value={username}/>
+                            <label htmlFor="user">User Name</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input id="password" className="form-control" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+                            <label htmlFor="password">Password</label>
+                        </div>
+                        <p><button type="submit" onSubmit={handleSubmit}>Login</button></p>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 }
