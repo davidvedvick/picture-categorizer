f0dacac1569cdd28fe66ae6c921cbe5b6b476634
David Vedvick <dvedvick@gmail.com>
2022-12-01 07:44:26 -0600

    Improve login styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import {UserLogin} from "./Users/UserLogin";
 import {BrowserRouter as Router, NavLink, Route, Routes} from "react-router-dom";
 
@@ -7,13 +7,13 @@ function App() {
     return (
         <Router>
             <div>
-                <nav className="navbar navbar-expand-lg bg-light">
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
                     <div className="container-fluid">
                         <NavLink className="navbar-brand" to="/">Cat Pics!</NavLink>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <NavLink className="navbar-brand" to="/">Home</NavLink>
+                                    <NavLink className="navbar-brand nav-link" to="/">Home</NavLink>
                                 </li>
                                 <li className="nav-item">
                                     <NavLink className="nav-link" to="/pictures">Browse Pictures</NavLink>
@@ -27,22 +27,13 @@ function App() {
                 </nav>
 
 
-                <hr />
-
-                {/*
-                      A <Switch> looks through all its children <Route>
-                      elements and renders the first one whose path
-                      matches the current URL. Use a <Switch> any time
-                      you have multiple routes, but you want only one
-                      of them to render at a time
-                    */}
-                <Routes>
-                    <Route path="/">
-                    </Route>
-                    <Route path="/pictures">
-                    </Route>
-                    <Route path="/user" element={<UserLogin/>} />
-                </Routes>
+                <div className="container mt-3">
+                    <Routes>
+                        <Route path="/" />
+                        <Route path="/pictures" />
+                        <Route path="/user" element={<UserLogin/>} />
+                    </Routes>
+                </div>
             </div>
         </Router>
     );
--- frontend/src/Users/UserLogin.tsx
+++ frontend/src/Users/UserLogin.tsx
@@ -29,22 +29,20 @@ export function UserLogin() {
     }
 
     return (
-        <>
-            <div className="sm container-fluid">
-                <div className="sm">
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-floating mb-3">
-                            <input id="email" className="form-control" type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email}/>
-                            <label htmlFor="email">E-mail</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input id="password" className="form-control" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
-                            <label htmlFor="password">Password</label>
-                        </div>
-                        <p><button type="submit" className="btn btn-primary mb-3">Login</button></p>
-                    </form>
-                </div>
+        <div className="col-sm-12">
+            <div className="card card-container">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-floating mb-3">
+                        <input id="email" className="form-control" type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                        <label htmlFor="email">E-mail</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input id="password" className="form-control" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+                        <label htmlFor="password">Password</label>
+                    </div>
+                    <p><button type="submit" className="btn btn-primary mb-3">Login</button></p>
+                </form>
             </div>
-        </>
+        </div>
     );
 }
