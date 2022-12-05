648ae54e751c8144386bcdad79eda80b11e35054
David Vedvick <dvedvick@gmail.com>
2022-12-05 07:32:48 -0600

    Make `loadMorePicturesIfNecessary` local to effect

--- frontend/src/Pictures/PictureList.tsx
+++ frontend/src/Pictures/PictureList.tsx
@@ -9,37 +9,37 @@ export function PictureList() {
     const pageNumberRef = React.useRef(0)
     const [pictures, setItemPictures] = React.useState<Picture[]>([]);
 
-    function loadingThreshold() {
-        const fifthLastNote = document.querySelector('div.pictures div.picture:nth-last-child(5)');
-        if (!fifthLastNote) return -1;
-
-        const rect = fifthLastNote.getBoundingClientRect();
-        return rect.top + window.scrollY;
-    }
-
-    async function loadMorePicturesIfNecessary() {
-        if (window.scrollY < loadingThreshold()) return;
-
-        window.removeEventListener('scroll', loadMorePicturesIfNecessary);
-
-        let isFinished = false;
-        try {
-            const response = await fetch(`/api/pictures?sort=id,desc&page=${pageNumberRef.current}&size=${pageSize}`);
-            if (response.ok) {
-                const page = await response.json() as Page<Picture>;
-                setItemPictures(prev => prev.concat(page.content));
-                pageNumberRef.current += 1;
-                isFinished = page.last;
-            }
-        } catch (error) {
-            console.error("There was an error getting item pictures", error);
-        } finally {
-            if (!isFinished)
-                window.addEventListener('scroll', loadMorePicturesIfNecessary);
-        }
-    }
-
     useEffect(() => {
+        function loadingThreshold() {
+            const fifthLastNote = document.querySelector('div.pictures div.picture:nth-last-child(5)');
+            if (!fifthLastNote) return -1;
+
+            const rect = fifthLastNote.getBoundingClientRect();
+            return rect.top + window.scrollY;
+        }
+
+        async function loadMorePicturesIfNecessary() {
+            if (window.scrollY < loadingThreshold()) return;
+
+            window.removeEventListener('scroll', loadMorePicturesIfNecessary);
+
+            let isFinished = false;
+            try {
+                const response = await fetch(`/api/pictures?sort=id,desc&page=${pageNumberRef.current}&size=${pageSize}`);
+                if (response.ok) {
+                    const page = await response.json() as Page<Picture>;
+                    setItemPictures(prev => prev.concat(page.content));
+                    pageNumberRef.current += 1;
+                    isFinished = page.last;
+                }
+            } catch (error) {
+                console.error("There was an error getting item pictures", error);
+            } finally {
+                if (!isFinished)
+                    window.addEventListener('scroll', loadMorePicturesIfNecessary);
+            }
+        }
+
         window.addEventListener('scroll', loadMorePicturesIfNecessary);
         loadMorePicturesIfNecessary();
         return () => window.removeEventListener('scroll', loadMorePicturesIfNecessary);
