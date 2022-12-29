a5a5dc14873934fa70e768555641bca94085b2b8
David Vedvick <dvedvick@gmail.com>
2022-12-29 10:19:13 -0600

    Move upload button to banner

--- frontend/src/Pictures/PictureList.tsx
+++ frontend/src/Pictures/PictureList.tsx
@@ -24,57 +24,55 @@ class UpdatedPictureListState implements PictureListState {
     }
 }
 
-export class PictureList extends React.Component<PictureListProperties, PictureListState> {
-    private readonly loadMorePicturesIfNecessary: () => Promise<void>;
+// Define locally to hide function
+function loadingThreshold() {
+    const fifthLastNote = document.querySelector('div.pictures div.picture:nth-last-child(5)');
+    if (!fifthLastNote) return -1;
 
-    constructor(props: PictureListProperties) {
-        super(props);
+    const rect = fifthLastNote.getBoundingClientRect();
+    return rect.top + window.scrollY;
+}
 
-        function loadingThreshold() {
-            const fifthLastNote = document.querySelector('div.pictures div.picture:nth-last-child(5)');
-            if (!fifthLastNote) return -1;
+function loadMorePicturesIfNecessaryForPictureList(pictureList: PictureList) {
+    const loadedPictures = new Set<number>();
+    let nextPageNumber = 0;
 
-            const rect = fifthLastNote.getBoundingClientRect();
-            return rect.top + window.scrollY;
+    return async function loadMorePicturesIfNecessary() {
+        if (window.scrollY < loadingThreshold()) return;
+
+        window.removeEventListener('scroll', loadMorePicturesIfNecessary);
+
+        let isFinished = false;
+        pictureList.setState(prev => new UpdatedPictureListState(true, prev?.pictures || []));
+        try {
+            const response = await fetch(`/api/pictures?sort=id,desc&page=${nextPageNumber}&size=${pageSize}`);
+            if (!response.ok) return;
+
+            const page = await response.json() as Page<Picture>;
+            pictureList.setState((prev) => new UpdatedPictureListState(prev.isLoading, prev.pictures.concat(page.content.filter(p => {
+                if (loadedPictures.has(p.id)) return false;
+                loadedPictures.add(p.id);
+                return true;
+            }))));
+            nextPageNumber = Math.max(nextPageNumber, page.number + 1);
+            isFinished = page.last;
+        } catch (error) {
+            console.error("There was an error getting item pictures", error);
+        } finally {
+            pictureList.setState(prev => new UpdatedPictureListState(false, prev.pictures));
+            if (!isFinished)
+                window.addEventListener('scroll', loadMorePicturesIfNecessary);
         }
-
-        const loadedPictures = new Set<number>();
-        let pageNumber = 0;
-
-        const loadMorePicturesIfNecessary = async () => {
-            if (window.scrollY < loadingThreshold()) return;
-
-            window.removeEventListener('scroll', loadMorePicturesIfNecessary);
-
-            let isFinished = false;
-            this.setState(prev => new UpdatedPictureListState(true, prev?.pictures || []));
-            try {
-                const response = await fetch(`/api/pictures?sort=id,desc&page=${pageNumber}&size=${pageSize}`);
-                if (response.ok) {
-                    const page = await response.json() as Page<Picture>;
-                    this.setState((prev) => new UpdatedPictureListState(prev.isLoading, prev.pictures.concat(page.content.filter(p => {
-                        if (loadedPictures.has(p.id)) return false;
-                        loadedPictures.add(p.id);
-                        return true;
-                    }))));
-                    pageNumber += 1;
-                    isFinished = page.last;
-                }
-            } catch (error) {
-                console.error("There was an error getting item pictures", error);
-            } finally {
-                this.setState(prev => new UpdatedPictureListState( false, prev.pictures));
-                if (!isFinished)
-                    window.addEventListener('scroll', loadMorePicturesIfNecessary);
-            }
-        }
-
-        this.loadMorePicturesIfNecessary = loadMorePicturesIfNecessary;
     }
+}
+
+export class PictureList extends React.Component<PictureListProperties, PictureListState> {
+
+    private readonly loadMorePicturesIfNecessary = loadMorePicturesIfNecessaryForPictureList(this);
 
     componentDidMount() {
         this.setState((_, props) => new UpdatedPictureListState(false, props.initialPictureList || []));
-        this.loadMorePicturesIfNecessary();
+        this.loadMorePicturesIfNecessary().catch(error => console.error("There was an error getting item pictures", error));
     }
 
     render() {
@@ -88,7 +86,7 @@ export class PictureList extends React.Component<PictureListProperties, PictureL
                     </div>
                 </div>
             ))}
-            {isLoading ? <div className="spinner-border center" role="status">
+            {isLoading ? <div className="spinner-border text-center" role="status">
                 <span className="visually-hidden">Loading...</span>
             </div> : null}
         </div>;
