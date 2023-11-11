dd393d34c96063387c4dbf3a23c3085b2f64665d
David <dvedvick@gmail.com>
2023-11-11 16:00:35 -0600

    Fix deployments and builds

--- deploy-to-nfsn.sh
+++ deploy-to-nfsn.sh
@@ -4,6 +4,19 @@ rm -rf _artifacts
 
 DOCKER_BUILDKIT=1 docker build --target staging -o _artifacts .
 
-scp _artifacts/catpics.jar "$USER"@"$SSH_HOST":/home/protected
+EXIT_CODE=${PIPESTATUS[0]}
 
-ssh "$USER"@"$SSH_HOST" -t 'nfsn signal-daemon App term'
+if [ "$EXIT_CODE" -ne 0 ]; then
+  exit "${EXIT_CODE}"
+fi
+
+chmod 755 _artifacts
+
+rsync -avzh --delete --log-file=rsync-log --exclude app-config.json \
+  ./_artifacts/ "$SSH_USER"@"$SSH_HOST":/home/protected
+
+ssh "$SSH_USER"@"$SSH_HOST" -t 'nfsn signal-daemon App term'
+
+EXIT_CODE=${PIPESTATUS[0]}
+
+exit "${EXIT_CODE}"
