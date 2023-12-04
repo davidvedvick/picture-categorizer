8ed24767d898603a0ab7f50b88fe83dd24560521
David Vedvick <dvedvick@gmail.com>
2023-12-04 10:14:59 -0600

    Make tags read-only when logged out

--- frontend/src/Pictures/Tags/PictureTagList.tsx
+++ frontend/src/Pictures/Tags/PictureTagList.tsx
@@ -2,6 +2,8 @@ import {PictureTag} from "./PictureTag";
 import {NewPictureTag} from "./NewPictureTag";
 import React from "react";
 import {Tag} from "../../../../transfer";
+import {instance as auth} from "../../Security/AuthorizationService";
+import {ReadOnlyPictureTag} from "./ReadOnlyPictureTag";
 
 interface PictureTagListProps {
     pictureId: number;
@@ -10,6 +12,7 @@ interface PictureTagListProps {
 export function PictureTagList(props: PictureTagListProps) {
     const {pictureId} = props;
     const [tags, setTags] = React.useState<Tag[]>([]);
+    const isLoggedIn = auth().getUserToken();
 
     async function updateTags(pictureId: number) {
         const response = await fetch(`/api/pictures/${pictureId}/tags`);
@@ -21,8 +24,12 @@ export function PictureTagList(props: PictureTagListProps) {
         updateTags(pictureId);
     }, [pictureId]);
 
-    return <div>
-        {tags.map(t => (<PictureTag {...t} {...props} onTagDeleted={() => updateTags(pictureId)} />))}
-        <NewPictureTag {...props} onNewPictureAdded={() => updateTags(pictureId)} />
-    </div>;
+    return isLoggedIn
+        ? <div>
+            {tags.map(t => (<PictureTag {...t} {...props} onTagDeleted={() => updateTags(pictureId)} />))}
+            <NewPictureTag {...props} onNewPictureAdded={() => updateTags(pictureId)} />
+        </div>
+        : <div>
+            {tags.map(t => (<ReadOnlyPictureTag {...t} {...props} />))}
+        </div>;
 }
---
+++ frontend/src/Pictures/Tags/ReadOnlyPictureTag.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Tag} from "../../../../transfer";
+
+interface PictureTagProps extends Tag {
+    pictureId: number;
+}
+
+export function ReadOnlyPictureTag(props: PictureTagProps) {
+    return <button type="button" className="btn btn-primary disabled">{props.tag}</button>;
+}
