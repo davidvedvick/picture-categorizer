c4cf4925d868a7707ce156dd92d6cf501d62b911
David <dvedvick@gmail.com>
2023-12-29 00:49:17 -0600

    Fix keys and data types

--- server/MySqlToSqLite.ts
+++ server/MySqlToSqLite.ts
@@ -5,6 +5,8 @@ import Database from "better-sqlite3";
 import { PictureRepositoryMySql, PictureRepositorySqLite } from "./pictures/PictureRepository.js";
 import { PictureTagRepositoryMySql, PictureTagRepositorySqLite } from "./pictures/tags/PictureTagRepository.js";
 import migrator from "./migrator.js";
+import CatEmployee from "./users/CatEmployee.js";
+import { Picture } from "./pictures/Picture.js";
 
 const pool = mysql.createPool(config.db);
 const catEmployeesMySql = new CatEmployeeRepositoryMySql(pool);
@@ -20,11 +22,29 @@ const pictureTagsSqLite = new PictureTagRepositorySqLite(database);
     await migrator(database);
 
     const catEmployees = await catEmployeesMySql.getAll();
-    for (const catEmployee of catEmployees) await catEmployeeSqLite.save(catEmployee);
+    const catEmployeesMap = new Map<number, CatEmployee>();
+    for (const catEmployee of catEmployees) {
+        catEmployeesMap.set(catEmployee.id, await catEmployeeSqLite.save(catEmployee));
+    }
 
     const pictures = await picturesMySql.findAll(null, null);
-    for (const picture of pictures) await picturesSqLite.save(picture);
+    const picturesMap = new Map<number, Picture>();
+    for (const picture of pictures) {
+        const newCatEmployee = catEmployeesMap.get(picture.catEmployeeId);
+        if (newCatEmployee) picture.catEmployeeId = newCatEmployee.id;
+
+        picture.file = await picturesMySql.findFileById(picture.id);
+
+        picturesMap.set(picture.id, await picturesSqLite.save(picture));
+    }
 
     const pictureTags = await pictureTagsMySql.getAll();
-    for (const pictureTag of pictureTags) await pictureTagsSqLite.addTag(pictureTag.pictureId, pictureTag.tag);
+    for (const pictureTag of pictureTags) {
+        const newPicture = picturesMap.get(pictureTag.pictureId);
+        if (newPicture) pictureTag.pictureId = newPicture.id;
+
+        await pictureTagsSqLite.addTag(pictureTag.pictureId, pictureTag.tag);
+    }
+
+    process.exit();
 })();
--- server/migrations/V01__sqlite_catpics.sql
+++ server/migrations/V01__sqlite_catpics.sql
@@ -1,6 +1,6 @@
 create table if not exists cat_employee
 (
-    id MEDIUMINT AUTO_INCREMENT UNIQUE primary key,
+    id INTEGER primary key,
     email      varchar(120) UNIQUE,
     password   varchar(128),
     is_enabled boolean default false
@@ -8,11 +8,11 @@ create table if not exists cat_employee
 
 create table if not exists picture
 (
-    id  MEDIUMINT AUTO_INCREMENT primary key,
-    cat_employee_id MEDIUMINT NOT NULL,
+    id  INTEGER primary key,
+    cat_employee_id INTEGER NOT NULL,
     file_name varchar(255) NOT NULL,
     mime_type varchar(50),
-    `file` LONGBLOB,
+    `file` BLOB,
     FOREIGN KEY (cat_employee_id) references cat_employee(`id`)
         ON DELETE RESTRICT
         ON UPDATE RESTRICT,
@@ -21,14 +21,14 @@ create table if not exists picture
 
 create table if not exists tag
 (
-    id MEDIUMINT AUTO_INCREMENT primary key,
+    id INTEGER primary key,
     tag varchar(50) UNIQUE
 );
 
 create table if not exists picture_tag
 (
-    picture_id MEDIUMINT,
-    tag_id MEDIUMINT,
+    picture_id INTEGER,
+    tag_id INTEGER,
     FOREIGN KEY (`picture_id`) REFERENCES picture(`id`)
         ON DELETE RESTRICT
         ON UPDATE RESTRICT,
